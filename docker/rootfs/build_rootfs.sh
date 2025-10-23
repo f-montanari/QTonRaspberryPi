5c163fd3d5263200848bca86891cdbb35ff77ad6
Franco Montanari <franco.montanari@kretz.com.ar>
2025-10-23 17:12:02 -0300

    Fix [RootFS]: Fix relative pathing in build script

--- docker/rootfs/build_rootfs.sh
+++ docker/rootfs/build_rootfs.sh
@@ -1,3 +1,2 @@
-docker buildx build --platform linux/arm64 --load -f docker/rootfs/Dockerfile -t crossbuild-rootfs .
-docker create -v ./docker/rootfs/build:/build --name cross-rootfs crossbuild-rootfs
-cp docker/rootfs/build/rasp.tar.gz ./rasp.tar.gz
+docker buildx build --platform linux/arm64 --load -t crossbuild-rootfs .
+docker create -v ./build:/build --name cross-rootfs crossbuild-rootfs
