0aa88b3ef173f405133d28ffe48157297b1d89d0
Franco Montanari <franco.montanari@kretz.com.ar>
2025-11-12 09:20:43 -0300

    Add [Shell]: Compiles whole project and copies it from inside container

--- build_all.sh
+++ build_all.sh
@@ -7,3 +7,6 @@ docker build -t crossbuild-base:latest -f docker/base/Dockerfile .
 docker build -t crossbuild-qt:latest -f docker/qt/Dockerfile --build-arg BUILD_OPENCV=OFF .
 
 docker build -t myproject-builder:latest -f docker/project-builder/Dockerfile .
+docker create --name cross-output myproject-builder
+docker cp cross-output:/build/project/HelloQt6 ./output/HelloQt6
+docker rm cross-output
--- docker/rootfs/build_rootfs.sh
+++ docker/rootfs/build_rootfs.sh
@@ -1,3 +1,7 @@
+docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
+# Do these two only once.
+# docker buildx create --use --name mybuilder
+# docker buildx inspect mybuilder --bootstrap
 docker buildx build --platform linux/arm64 --load -t crossbuild-rootfs .
 docker create --name cross-rootfs crossbuild-rootfs
 docker cp cross-rootfs:/build.log ./build/build.log
